446dcfbf61a693d9263d0d2ab8b7301ce9fc2eaf
曾晓江 <zengxj@genvict.com>
2020-11-19 15:07:10 +0800

    1：水波纹效果测试；

--- app/src/main/java/com/genvict/widget/WaveViewActivity.java
+++ app/src/main/java/com/genvict/widget/WaveViewActivity.java
@@ -1,6 +1,9 @@
 package com.genvict.widget;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.genvict.widgets.jwaveloading.WaveViewBySinCos;
 
@@ -9,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class WaveViewActivity extends AppCompatActivity {
     private WaveViewBySinCos waveTest1;
     private WaveViewBySinCos waveTest2;
+    private ImageView ivShip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +22,10 @@ public class WaveViewActivity extends AppCompatActivity {
         waveTest2 = findViewById(R.id.wave_test2);
         waveTest1.startAnimation();
         waveTest2.startAnimation();
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.ship_anim);
+        ivShip = findViewById(R.id.iv_ship);
+        ivShip.startAnimation(animation);
     }
 
 
